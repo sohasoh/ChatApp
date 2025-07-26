3f2777f6144704d3b5cd9c64af2861afc6d8cffc
sohasoh <soh.silas@giortech.com>
2025-07-26 13:03:49 +0200

    duyguihw

---
+++ Backend/myproject/myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .firebase import database
+from django.http import JsonResponse
+
+# Create your views here.
+
+def home(request):
+    # Exemple : Aouter des données à Firebase
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        stack = request.POST.get('stack')
+        data = {"name": name, "stack": stack}
+        database.child("users").push(data) # Ajoute les données dans la collection "users"
+    
+    # Exemple : Lire des données depuis Firebase
+    users = database.child("users").get().val() # Récupère tous les utilisateurs
+    return render(request, 'myapp/index.html', {'users': users})
+# Create your views here.
